edacaa92b210696c25aeab5f59f6bec0638e2a45
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-10-24 19:17:05 -0300

    feat: add new component

--- App.tsx
+++ App.tsx
@@ -3,13 +3,18 @@ import { StyleSheet, View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
 import Text from './src/common/components/text';
+import Spacer from './src/common/components/Spacer/index';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <View style={styles.container}>
-        <Text size={50} color="brand">
-          Open up App.tsx to start working on your app!
+        <Text size={30} color="brand">
+          Meu componente 1
+        </Text>
+        <Spacer height={20} />
+        <Text size={30} color="brand">
+          Meu componente 2
         </Text>
         <StatusBar style="auto" />
       </View>
