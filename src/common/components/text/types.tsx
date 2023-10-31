e7bfb93d309fe62ec645994f61fdd8748e888b22
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-10-31 20:35:49 -0300

    feat: deleted duplicated files

--- src/common/components/text/types.tsx
+++
@@ -1,7 +0,0 @@
-import { TextProps } from 'react-native';
-import { ColorPalette } from 'styled-components/native';
-
-export interface Props extends TextProps {
-  color?: ColorPalette;
-  size?: number;
-}
