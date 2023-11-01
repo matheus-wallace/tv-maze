5b540e929bbe37c502e29999068c2b00411f2699
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-10-31 22:06:53 -0300

    feat: import safe area view

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
-import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
-import Text from './src/common/components/Text';
-import Spacer from './src/common/components/Spacer/index';
 import Container from './src/common/components/Container/';
+import Content from './src/common/components/Content';
+import Spacer from './src/common/components/Spacer/index';
+import Text from './src/common/components/Text';
+import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container>
         <Text size={30} color="onSecondary">
-          Meu componente 1
+          1 Meu componente de text
         </Text>
-        <Spacer height={20} />
+        <Spacer height={30} />
         <Text size={30} color="onSecondary">
-          Meu componente 2
+          2 Meu componente de text
         </Text>
-        <StatusBar style="auto" />
+        <Content>
+          <Spacer height={30} />
+          <Text size={30} color="onSecondary">
+            1 Meu componente de text
+          </Text>
+          <Spacer height={30} />
+          <Text size={30} color="onSecondary">
+            2 Meu componente de text
+          </Text>
+        </Content>
       </Container>
     </ThemeProvider>
   );
