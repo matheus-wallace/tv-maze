43a385b638b88c096258cfecd15f511f550ffa72
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-11-13 10:35:07 -0300

    feat: new component that fetch url and get cover film

--- App.tsx
+++ App.tsx
@@ -9,6 +9,8 @@ import Avatar from './src/common/components/Avatar';
 import BackButton from './src/common/components/BackButton';
 import Notfound from './src/common/components/NotFound';
 import Input from './src/common/components/Input';
+import ShowCover from './src/common/components/ShowCover';
+import { View } from 'react-native';
 
 export default function App() {
   return (
@@ -26,6 +28,20 @@ export default function App() {
           <Text size={30} color="onSecondary">
             1 Meu componente de text
           </Text>
+          <View
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+          >
+            <ShowCover
+              onPress={() => {}}
+              title="The flash"
+              url="https://static.tvmaze.com/uploads/images/medium_portrait/1/4600.jpg"
+            />
+            <ShowCover
+              onPress={() => {}}
+              title="The flash"
+              url="https://static.tvmaze.com/uploads/images/medium_portrait/1/4600.jpg"
+            />
+          </View>
           <Spacer height={30} />
           <Icon icon="home" />
           <Avatar size="small" />
---
+++ src/common/components/ShowCover/index.tsx
@@ -0,0 +1,67 @@
+import React, { useMemo } from 'react';
+import { Button, Pressable, useWindowDimensions, View } from 'react-native';
+import { useTheme } from 'styled-components/native';
+import Icon from '../icon';
+import {
+  Container,
+  AbsoluteIcon,
+  CenterIconAndImage,
+  ShowCoverImage,
+} from './styles';
+
+import { ShowCoverProps } from './types';
+import Spacer from '../Spacer';
+import Text from '../Text';
+
+const aspectRatio = 9 / 12.5;
+const widthPercentage = 0.45;
+const ShowCover = ({ onPress, title, url }: ShowCoverProps) => {
+  /**
+   * Hooks
+   */
+  const { colors, spacing } = useTheme();
+  const { width } = useWindowDimensions();
+
+  /**
+   * Memos
+   */
+
+  const widthShowCover = useMemo(() => width * widthPercentage, [width]);
+  return (
+    <Pressable
+      onPress={onPress}
+      accessibilityLabel={title}
+      accessibilityRole="button"
+    >
+      <Container width={widthShowCover}>
+        <CenterIconAndImage>
+          <ShowCoverImage
+            hasBackground={!url}
+            source={{ uri: url }}
+            resizeMode="contain"
+            style={{
+              aspectRatio,
+              width: widthShowCover,
+            }}
+          />
+          {!url && (
+            <AbsoluteIcon>
+              <Icon icon="picture" size={40} color={colors.caption}></Icon>
+            </AbsoluteIcon>
+          )}
+        </CenterIconAndImage>
+
+        {!title && (
+          <>
+            <Spacer height={spacing.sm} />
+            <Text numberOfLines={1} color="caption">
+              {title}
+            </Text>
+          </>
+        )}
+      </Container>
+    </Pressable>
+  );
+};
+
+export default React.memo(ShowCover);
