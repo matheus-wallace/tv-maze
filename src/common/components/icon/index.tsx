9c1887fe1570a61eec5c758fe78fdfbbe4a7eba0
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-11-23 08:33:42 -0300

    fix: remove the import of

--- src/common/components/icon/index.tsx
+++ src/common/components/icon/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
 import { IconProps } from './types';
 import { Container } from './styles';
 import Icons from '../../constants/icons';
 
-const Icon = ({ icon, size = 20, color, style, ...rest }: Omit<IconProps, 'source'>) => {
+const Icon = ({
+  icon,
+  size = 20,
+  color,
+  style,
+  ...rest
+}: Omit<IconProps, 'source'>) => {
   return (
-    <Container size={size} source={Icons[icon]} {...rest} style={[{ tintColor: color }, style]} resizeMode="contain" />
+    <Container
+      size={size}
+      source={Icons[icon]}
+      {...rest}
+      style={[{ tintColor: color }, style]}
+      resizeMode="contain"
+    />
   );
 };
 
