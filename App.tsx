9490bf53ce07ba625aaa555e4d07a945cc967a92
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-11-24 16:53:46 -0300

    feat: remove some test components and importing the card episode

--- App.tsx
+++ App.tsx
@@ -1,53 +1,34 @@
 import { ThemeProvider } from 'styled-components/native';
-import Container from './src/common/components/Container/';
-import Content from './src/common/components/Content';
-import Spacer from './src/common/components/Spacer/index';
-import Text from './src/common/components/Text';
-import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
-import Icon from './src/common/components/icon';
-import Avatar from './src/common/components/Avatar';
 import BackButton from './src/common/components/BackButton';
-import Notfound from './src/common/components/NotFound';
+import Container from './src/common/components/Container';
+import Content from './src/common/components/Content';
+import EpisodeCard from './src/common/components/EpisodeCard';
 import Input from './src/common/components/Input';
-import ShowCover from './src/common/components/ShowCover';
-import { View } from 'react-native';
-import EpisodeCover from './src/common/components/EpisodeCover';
+import Spacer from './src/common/components/Spacer';
+import Text from './src/common/components/Text';
+import { episodeMocked } from './src/common/constants/mocks/episode.mock';
+import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container>
         <Content>
+          <Spacer height={20} />
           <BackButton
             onPress={() => {
-              console.log('clicou');
+              console.log('clicou no botão in');
             }}
           />
-          <Spacer height={15} />
+          <Spacer height={20} />
           <Input loading />
-
-          <Text size={30} color="onSecondary">
-            1 Meu componente de text
+          <Spacer height={20} />
+          <Text numberOfLines={1} size={30} color="onSecondary">
+            2 Meu componente de text 2 Meu componente de text2 Meu componente de
+            text2 Meu componente de text
           </Text>
-          <View
-            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
-          >
-            <ShowCover
-              onPress={() => {}}
-              title="The flash"
-              url="https://static.tvmaze.com/uploads/images/medium_portrait/1/4600.jpg"
-            />
-            <ShowCover
-              onPress={() => {}}
-              title="The flash"
-              url="https://static.tvmaze.com/uploads/images/medium_portrait/1/4600.jpg"
-            />
-          </View>
-          <Spacer height={30} />
-          <Icon icon="home" />
-          <Avatar size="small" />
-          <EpisodeCover url="https://static.tvmaze.com/uploads/images/medium_landscape/15/38639.jpg" />
-          <Notfound />
+
+          <EpisodeCard episode={episodeMocked}></EpisodeCard>
         </Content>
       </Container>
     </ThemeProvider>
