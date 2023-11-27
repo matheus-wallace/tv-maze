7854a76cb61e54fd1615d4c403d365e6355619df
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2023-11-27 17:40:28 -0300

    feat: new home screen

---
+++ src/screens/Home/home.view.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'pink' }}>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Home;
