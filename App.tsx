64a5cd835bf31375a1eb71afc8e9330f52a2d5d6
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-01-23 07:51:51 -0300

    feat: removing importings and add routes components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components/native';
-import BackButton from './src/common/components/BackButton';
-import Container from './src/common/components/Container';
-import Content from './src/common/components/Content';
-import EpisodeCard from './src/common/components/EpisodeCard';
-import Input from './src/common/components/Input';
-import Spacer from './src/common/components/Spacer';
-import Text from './src/common/components/Text';
-import { episodeMocked } from './src/common/constants/mocks/episode.mock';
 import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
-import BottomTabNavigator from './src/navigation/BottomTabNavigator';
+import Routes from './src/navigation';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <NavigationContainer>
-        <BottomTabNavigator />
+        <Routes />
       </NavigationContainer>
     </ThemeProvider>
   );
